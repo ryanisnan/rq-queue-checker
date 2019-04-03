0ac142806fc6bfae327c342a1e71f4bb80ca1dc0
ryanisnan <ryanisnan@gmail.com>
2019-04-03 14:46:57 -0700

    lots of core improvements, modularize code

--- src/run.py
+++ src/run.py
@@ -1,4 +1,5 @@
 from rq import Queue
+import copy
 import json
 import logging
 import os
@@ -9,12 +10,13 @@ import uuid
 
 
 base_logger = logging.getLogger(__name__)
-base_logger.setLevel(logging.DEBUG)
+base_logger.setLevel(int(os.environ.get('LOGLEVEL', logging.DEBUG)))
 
 handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.DEBUG)
+handler.setLevel(int(os.environ.get('LOGLEVEL', logging.DEBUG)))
 
 base_logger.addHandler(handler)
+logger = copy.deepcopy(base_logger)
 
 
 class LoggingTransactionAdapter(logging.LoggerAdapter):
@@ -32,75 +34,94 @@ class LoggingTransactionAdapter(logging.LoggerAdapter):
         return '[trn: %s] %s' % (getattr(self, 'trn_id'), msg), kwargs
 
 
-def get_queues_and_thresholds(logger):
+def get_redis_connection(redis_host, redis_port):
     """
-    Returns a list of 2-tuples, where the first item is the queue name and the
-    second item is the notification threshold.
+    Build and return a redis connection based on the passed-in host and port.
+    Verifies that the connection is valid or aborts the script.
     """
-    QUEUE_THRESHOLDS = os.environ.get('QUEUE_THRESHOLDS', '{}')
-    logger.debug('Queue configuration: %s' % QUEUE_THRESHOLDS)
+    logger.debug('Redis connection info: %s:%s' % (redis_host, redis_port))
+
+    r = redis.Redis(host=redis_host, port=redis_port, db=0)
+
+    # Ensure connection to redis is good
+    r.ping()
+    logger.debug('Connected to redis')
+
+    return r
+
+
+def get_queue_config(serialized_json):
+    """
+    Returns a python dict containing queue configuration.
+    """
+    logger.debug('Queue configuration: %s' % serialized_json)
 
     try:
-        queues_and_thresholds = json.loads(QUEUE_THRESHOLDS)
+        queue_config = json.loads(serialized_json)
     except:
-        logger.error('Could not parse queue configuration as JSON')
-        exit()
+        raise ValueError('Could not parse queue configuration as JSON')
 
-    return queues_and_thresholds.items()
+    return queue_config
+
+
+def process_queue(queue_name, threshold):
+    """
+    Given a queue name and a threshold figure, will attempt to check if
+    the length of the queue is above the threshold. If so, it will attempt to
+    send an alert.
+
+    Returns True if an alert was triggered, and False if not.
+    """
+    r = get_redis_connection(os.environ.get('REDIS_HOST', 'localhost'), os.environ.get('REDIS_PORT', '6379'))
+
+    queue = Queue(queue_name, connection=r)
+    length = len(queue)
+
+    if length >= threshold:
+        send_alert(queue_name, length)
+        return True
+
+    return False
+
+
+def send_alert(queue_name, queue_length):
+    """
+    Send an alert via slack to a specific webhook address notifying users of the
+    queue length.
+
+    Returns True if an alert was sent. False if not.
+    """
+    SLACK_WEBHOOK_URL = os.environ.get('SLACK_WEBHOOK_URL')
+
+    if not SLACK_WEBHOOK_URL:
+        logger.warning('SLACK_WEBHOOK_URL was not found in the environment, no alert will be sent.')
+        return False
+
+    payload = {'text': '%s queue length alert - %d' % (queue_name, queue_length)}
+    requests.post(SLACK_WEBHOOK_URL, json=payload)
+
+    logger.debug('slack notification sent for %s' % queue_name)
+    return True
+
+
+def dummy_job():
+    """
+    Dummy job for use in the test suite.
+    """
+    pass
 
 
 def __main__():
     logger = LoggingTransactionAdapter(base_logger)
     logger.debug('Running queue checker')
 
-    # Fetch connection information - ensure it has been set
-    REDIS_HOST = os.environ.get('REDIS_HOST')
-    REDIS_PORT = os.environ.get('REDIS_PORT')
-    assert REDIS_HOST is not None
-    assert REDIS_PORT is not None
+    queue_config = get_queue_config(os.environ.get('QUEUE_CONFIG', '{}'))
 
-    logger.debug('Redis connection info: %s:%s' % (REDIS_HOST, REDIS_PORT))
-
-    # Ensure connection to redis is good
-    r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=0)
-    try:
-        r.ping()
-    except:
-        logger.error('Could not connect to redis, exiting')
-        exit()
-    else:
-        logger.debug('Connected to redis')
-
-    # Iterate the queues, and check the length
-    for queue_name, threshold in get_queues_and_thresholds(logger):
-        logger.debug('Checking %s queue' % queue_name)
-
-        q = Queue(queue_name, connection=r)
-        queue_length = len(q)
-
-        if queue_length >= threshold:
-            logger.debug('%s queue is over threshold (length=%d, threshold=%d)' % (queue_name, queue_length, threshold))
-            send_alert(logger, queue_name, queue_length)
+    for queue_name, threshold in queue_config.items():
+        process_queue(queue_name, threshold)
 
     logger.debug('Queue checker finished')
 
 
 if __name__ == "__main__":
     __main__()
-
-
-def send_alert(logger, queue_name, queue_length):
-    """
-    Send an alert via slack to a specific webhook address notifying users of the
-    queue length.
-    """
-    SLACK_WEBHOOK_URL = os.environ.get('SLACK_WEBHOOK_URL')
-
-    if not SLACK_WEBHOOK_URL:
-        logger.warning('SLACK_WEBHOOK_URL was not found in the environment, no alert will be sent.')
-        return
-
-    payload = {'text': '%s queue length alert - %d' % (queue_name, queue_length)}
-    requests.post(SLACK_WEBHOOK_URL, json=payload)
-
-    logger.debug('slack notification sent for %s' % queue_name)
